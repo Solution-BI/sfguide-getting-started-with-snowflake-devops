0842794485f47e31e5ad9e10f4f329f2c42a8988
Mustapha <93934739+mallaoui@users.noreply.github.com>
2025-02-16 14:47:30 +0000

    update 04

--- steps/04_orchestrate_jobs.sql
+++ steps/04_orchestrate_jobs.sql
@@ -16,8 +16,7 @@ create or alter table vacation_spots (
   , zoo_cnt int
   , korean_restaurant_cnt int
   -- STEP 5: INSERT CHANGES HERE
-data_retention_time_in_days = {{retention_time}};
-
+) data_retention_time_in_days = {{retention_time}};
 
 
 -- task to merge pipeline results into target table
